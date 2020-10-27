f8af9b0dec73056d03cd9def82ab8e2e2f023f6d
Axel Hörteborn <Axel.Horteborn@sspa.se>
2020-10-27 06:05:20 +0100

    Now return True/False if the key was found

--- pyAgriculture/sorting_utils.py
+++ pyAgriculture/sorting_utils.py
@@ -1,5 +1,6 @@
 def find_by_key(data_dict: dict, key_name: str, key_value: str):
+    """Returns a dict key where "key_name" equals the key_value """
     for key in data_dict.keys():
         if data_dict[key][key_name] == key_value:
-            return key
-    return key_value
+            return True, key
+    return False, key_value
